172447433ce5270b6bbb786796c9502316ebff6c
emily <emilysu36@gmail.com>
2023-06-20 22:11:29 -0700

    Add uncommitted src/main files

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,23 @@
+public class StringCalculator {
+    public static int add(String numbers) {
+        int returnValue = 0;
+        String[] numbersArray = numbers.split(",|\n");
+
+        if (numbersArray.length > 2) {
+            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+        }
+        else {
+            for (String number : numbersArray) {
+                if (!number.trim().isEmpty()) {
+                    if (Integer.parseInt(number.trim()) < 0) {
+                        throw new IllegalArgumentException("Negatives not allowed");
+                    }
+                    if (Integer.parseInt(number.trim()) <= 1000) {
+                        returnValue += Integer.parseInt(number.trim()); // If it is not a number, parseInt will throw an exception
+                    }
+                }
+            }
+        }
+        return returnValue;
+    }
+}
